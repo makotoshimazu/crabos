d47f9e3e19dc1aad0515a51fcf86e54414b28707
Toru Ogawa <Hakuyume@users.noreply.github.com>
2022-05-31 22:17:14 +0900

    hello world

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,22 @@
-fn main() {
-    println!("Hello, world!");
+#![no_std]
+#![no_main]
+
+use core::arch;
+use core::panic::PanicInfo;
+use core::ptr;
+
+arch::global_asm!(include_str!("../boot.s"));
+
+#[no_mangle]
+pub extern "C" fn __start_rust() -> ! {
+    for c in b"Hello World\n" {
+        unsafe { ptr::write_volatile(0x10000000 as *mut u8, *c) };
+    }
+
+    loop {}
+}
+
+#[panic_handler]
+fn panic(_: &PanicInfo) -> ! {
+    loop {}
 }
