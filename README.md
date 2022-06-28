bdc882672d3ffdd68305cf20240870c3350ab55a
Makoto Shimazu <makoto.shimaz@gmail.com>
2022-06-28 23:32:21 +0900

    Create /disk and generate startup.nsh there (#3)
    * Create /disk and generate startup.nsh there
    
    * twaek

--- README.md
+++ README.md
@@ -2,3 +2,10 @@
 
 Named after [Gazami](https://en.wikipedia.org/wiki/Portunus_trituberculatus)
 
+
+## How to run
+
+```shell
+cargo run
+# Build efi, creates /disk with some files to boot, and run QEMU with mounting /disk.
+```
--- qemu_run.sh
+++ qemu_run.sh
@@ -1,10 +1,20 @@
 #!/bin/sh
 
 script_dir=$(cd $(dirname $0); pwd)
+built_binary_path="$1"
 
+DISK_DIR=${script_dir}/disk
 OVMF_CODE=${script_dir}/ovmf/OVMF_CODE.fd
 OVMF_VARS=${script_dir}/ovmf/OVMF_VARS.fd
-BUILD_DIR=$(dirname $1)
+
+# Make sure the disk dir exists
+mkdir -p $DISK_DIR
+
+# Copy the binary to the disk dir
+cp $built_binary_path $DISK_DIR
+
+# Create startup.nsh for EDK2 Shell to read.
+echo "fs0:$(basename $built_binary_path)" > $DISK_DIR/startup.nsh
 
 qemu-system-x86_64 \
     -nodefaults \
@@ -14,5 +24,4 @@ qemu-system-x86_64 \
     -serial stdio \
     -drive if=pflash,format=raw,file=$OVMF_CODE,readonly=on \
     -drive if=pflash,format=raw,file=$OVMF_VARS,readonly=on \
-    -drive format=raw,file=fat:rw:$BUILD_DIR \
-
+    -drive format=raw,file=fat:rw:$DISK_DIR \
