06f9875fb3d3230b9cf06f092c1edf1c98a0a33a
Yuki Y <yukidmy@gmail.com>
2022-06-28 21:57:15 +0900

    UEFI application with OVMF

---
+++ qemu_run.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+OVMF_CODE=ovmf/OVMF_CODE.fd
+OVMF_VARS=ovmf/OVMF_VARS.fd
+BUILD_DIR=$(pwd)/target/x86_64-unknown-uefi/debug
+
+qemu-system-x86_64 \
+    -nodefaults \
+    -machine q35,accel=kvm:tcg \
+    -nographic \
+    -m 128M \
+    -drive if=pflash,format=raw,file=$OVMF_CODE,readonly=on \
+    -drive if=pflash,format=raw,file=$OVMF_VARS,readonly=on \
+    -drive format=raw,file=fat:rw:$BUILD_DIR \
+    -serial stdio \
