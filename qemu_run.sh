f2537f279c0f210c05e4dbd4c31e8253663685c2
Yuki Y <yukidmy@gmail.com>
2022-06-28 22:29:06 +0900

    Update build configs

--- qemu_run.sh
+++ qemu_run.sh
@@ -1,15 +1,18 @@
 #!/bin/sh
 
-OVMF_CODE=ovmf/OVMF_CODE.fd
-OVMF_VARS=ovmf/OVMF_VARS.fd
-BUILD_DIR=$(pwd)/target/x86_64-unknown-uefi/debug
+script_dir=$(cd $(dirname $0); pwd)
+
+OVMF_CODE=${script_dir}/ovmf/OVMF_CODE.fd
+OVMF_VARS=${script_dir}/ovmf/OVMF_VARS.fd
+BUILD_DIR=$(dirname $1)
 
 qemu-system-x86_64 \
     -nodefaults \
     -machine q35,accel=kvm:tcg \
     -nographic \
     -m 128M \
+    -serial stdio \
     -drive if=pflash,format=raw,file=$OVMF_CODE,readonly=on \
     -drive if=pflash,format=raw,file=$OVMF_VARS,readonly=on \
     -drive format=raw,file=fat:rw:$BUILD_DIR \
-    -serial stdio \
+
