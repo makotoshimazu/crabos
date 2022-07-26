ef6edcc1f3e6b6c5ba852abaf1f7d6b0ee72fc84
S.Naka <25317025+S-Nakamur-a@users.noreply.github.com>
2022-07-26 21:48:01 +0900

    allocate memory and load kernel file (#7)
    * allocate memory and load kernel file
    
    * remove unused module
    
    * remove comments
    
    * add comment for buffer size

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,11 @@
 #![no_std]
 #![no_main]
 #![feature(abi_efiapi)]
-use core::clone;
 use core::fmt::Write;
+use core::slice::from_raw_parts_mut;
 use uefi::prelude::*;
-use uefi::proto::media::file::{File, FileAttribute, FileMode};
+use uefi::proto::media::file::{File, FileAttribute, FileInfo, FileMode};
+use uefi::table::boot::{AllocateType, MemoryType};
 use uefi::CStr16;
 
 #[entry]
@@ -39,11 +40,30 @@ fn main(handle: Handle, mut system_table: SystemTable<Boot>) -> Status {
         };
         let mut file = handle.into_regular_file().unwrap();
         // TODO: allocate enough memory
-        let mut buffer = [0; 10000];
-        let nbytes = file.read(&mut buffer).unwrap();
+        let mut file_info_buffer = [0; 1000];
+        let file_info = file.get_info::<FileInfo>(&mut file_info_buffer).unwrap();
 
-        writeln!(system_table.stdout(), "{}", nbytes).unwrap();
+        const PAGE_UNIT_SIZE: usize = 0x1000;
+        let kernel_file_size = file_info.file_size() as usize;
+        let page_size = (kernel_file_size + PAGE_UNIT_SIZE - 0x1) / PAGE_UNIT_SIZE;
 
+        writeln!(system_table.stdout(), "{}", kernel_file_size).unwrap();
+
+        let addr = fs_sytem_table
+            .boot_services()
+            .allocate_pages(
+                AllocateType::Address(0x100000),
+                MemoryType::LOADER_DATA,
+                page_size,
+            )
+            .unwrap();
+        let buffer = from_raw_parts_mut(addr as *mut u8, page_size * PAGE_UNIT_SIZE);
+        file.read(buffer).unwrap();
+
+        let ptr = u64::from_ne_bytes(buffer[24..32].try_into().unwrap());
+        writeln!(system_table.stdout(), "{:?}", ptr as *const u64).unwrap();
+
+        // ↑↑↑↑↑ mikan book list3.2
         // TODO: Move the instruction pointer to the address stored at (buffer + 24).
     }
 
